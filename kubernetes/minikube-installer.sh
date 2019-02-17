50e442b1229869124d34a08600b590ffa16d7971
Mas <airtime166@yahoo.com>
2019-02-16 22:42:40 -0500

    Update minikube-installer.sh

--- kubernetes/minikube-installer.sh
+++ kubernetes/minikube-installer.sh
@@ -13,6 +13,7 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 
 cp minikube /usr/local/bin && rm minikube
 echo "setting up kubectl"
+
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -21,8 +22,16 @@ enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
 EOF
-yum install -y kubectl
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
 
 passwd 
 minikube delete
