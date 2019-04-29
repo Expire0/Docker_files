c763a12adbf071c08802c5a825cb95a25599ba46
Mas <airtime166@yahoo.com>
2019-04-29 10:31:34 -0400

    Create clean_install_fedora.sh

---
+++ clean_install_fedora.sh
@@ -0,0 +1,28 @@
+#https://docs.docker.com/v17.09/engine/installation/linux/docker-ce/centos/#install-using-the-repository
+## This clean script will install Docker from Docker's official repo instead of Centos or Fedora's
+##The script also removes yum and ensures DNF is installed. This is unrelated to Docker but I felt the need to do it. 
+
+
+yum install dnf
+
+dnf remove docker \
+                  docker-common \
+                  docker-selinux \
+                  docker-engine \
+                  yum \
+                  yum.utils
+
+
+dnf install -y dnf-utils \
+  dnf-plugins-core \
+  device-mapper-persistent-data \
+  lvm2
+
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+dnf config-manager  --enablerepo=docker-ce-edge
+
+
+dnf -y install docker-ce
+
+systemctl start docker && systemctl enable docker
