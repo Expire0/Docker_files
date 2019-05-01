90b26b72976f6a6535a783f9fe9f8715ece38361
Mas <airtime166@yahoo.com>
2019-05-01 13:46:59 -0400

    Update minikube-installer.sh

--- kubernetes/minikube-installer.sh
+++ kubernetes/minikube-installer.sh
@@ -32,11 +32,6 @@ dnf install -y dnf-plugins-core \
   device-mapper-persistent-data \
   lvm2
 
-echo "installing the kvm driver"
-dnf install libvirt-daemon-kvm qemu-kvm
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
-
 newgrp libvirt
 
 usermod -a -G libvirt $(whoami)
