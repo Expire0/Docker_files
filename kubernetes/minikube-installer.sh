8affd388fc720e1b2387508e5792ffe5424c6c92
Mas <airtime166@yahoo.com>
2019-05-01 13:42:41 -0400

    Update minikube-installer.sh

--- kubernetes/minikube-installer.sh
+++ kubernetes/minikube-installer.sh
@@ -37,8 +37,8 @@ dnf install libvirt-daemon-kvm qemu-kvm
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 sudo systemctl status libvirtd.service
-usermod -a -G libvirt $(whoami)
 newgrp libvirt
+usermod -a -G libvirt $(whoami)
 
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 \
