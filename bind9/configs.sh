5870c1b17b13e96eb9fe826a02e86d2e2449f98b
root <root@ip-172-26-3-128.us-east-2.compute.internal>
2019-09-28 23:38:07 +0000

    cleaning up

--- bind9/configs.sh
+++ bind9/configs.sh
@@ -1,7 +1,12 @@
 
 
+
+cd /root/
+rm -rf expire0_priv/ 
+ssh-agent bash -c ' ssh-add  /root/.ssh/id_git; git clone git@github.com:Expire0/expire0_priv.git'
+cd /root/Docker_files/bind9 
 cp ../../expire0_priv/bind-configs/configs.zip .
-unzip configs.zip 
+unzip -P $2 configs.zip 
 
 docker build -t powserve:dns$1 . 
 echo "images build complete. Cleaning up the repo"
