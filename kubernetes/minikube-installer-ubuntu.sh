500ee3e2289a0924ff64ed3107d6c08965233b5e
Mas <4345024+Expire0@users.noreply.github.com>
2021-05-02 13:04:58 -0400

    Update minikube-installer-ubuntu.sh

--- kubernetes/minikube-installer-ubuntu.sh
+++ kubernetes/minikube-installer-ubuntu.sh
@@ -43,6 +43,12 @@ sudo apt-get -y update
 sudo apt-get install -y kubectl
 apt -y autoremove
 
+echo "Installing Helm"
+wget https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz
+tar -zxvf helm-v3.5.3-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+helm repo add stable https://charts.helm.sh/stable
+
 minikube delete
 su - mini
 echo " user minikube start --vm-driver=docker to start minikube"
