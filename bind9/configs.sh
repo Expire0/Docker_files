3c478930507b391ecd90c49d9cfd5b67c9406e64
Mas <you@example.com>
2024-03-12 15:56:59 -0400

    minor update

--- bind9/configs.sh
+++ bind9/configs.sh
@@ -3,7 +3,7 @@
 
 cd /root/
 rm -rf expire0_priv/ 
-ssh-agent bash -c ' ssh-add  /root/.ssh/id_git; git clone git@github.com:Expire0/expire0_priv.git'
+ssh-agent bash -c ' ssh-add  /root/.ssh/id_rsa; git clone git@github.com:Expire0/expire0_priv.git'
 cd /root/Docker_files/bind9 
 cp ../../expire0_priv/bind-configs/configs.zip .
 unzip -P $2 configs.zip 
